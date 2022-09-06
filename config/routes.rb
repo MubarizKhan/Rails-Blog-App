2be7bd6c446a0bc46c5f2f04a91218465c3db61c
MubarizKhan <p176007@nu.edu.pk>
2022-09-06 18:55:10 +0500

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'articles#index'
+
+  resources :articles
+  get "/articles/index", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
+
+end
