8c33e05c39d0fd158157a4742a46a87819e90551
MubarizKhan <p176007@nu.edu.pk>
2022-09-09 17:33:11 +0500

    Added skip_before_action params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :require_login
   # protect_from_forgery with: :exception
   # before_action :configure_permitted_parameters, if: :devise_controller?
   # before_action :authenticate_user!
+
+  private
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to new_login_url # halts request cycle
+    end
+  end
 
   # def home; end
 
--- app/controllers/payment/invoices_controller.rb
+++ app/controllers/payment/invoices_controller.rb
@@ -1,5 +1,6 @@
 class Payment::InvoicesController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
+  skip_before_action :require_login, only: [:index]
 
   def index
     @invoices = Invoice.all
