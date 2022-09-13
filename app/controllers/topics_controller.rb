18cb88446c316a7d989699c801740e3237ce5501
MubarizKhan <p176007@nu.edu.pk>
2022-09-13 19:59:08 +0500

    added and completed topic AJAX functionality

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,38 @@
-# frozen_string_literal: true
-
 class TopicsController < ApplicationController
-  def index; end
+
+  def index
+    @topics = Topic.all
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+
+    respond_to do |format|
+      if @topic.save
+        format.html { redirect_to @topic, notice: 'Topic was successfully created.' }
+        format.js
+        format.json { render json: @topic, status: :created, location: @topic }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @topic.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def show
+    @article = Topic.find(params[:id])
+    redirect_to index
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:topic_name)
+  end
+
 end
