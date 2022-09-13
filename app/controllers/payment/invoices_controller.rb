d9bbb835e25c32412d91ab698fa3a556d90e23a8
MubarizKhan <p176007@nu.edu.pk>
2022-09-13 16:28:03 +0500

    added invoice add page

--- app/controllers/payment/invoices_controller.rb
+++ app/controllers/payment/invoices_controller.rb
@@ -10,6 +10,18 @@ class Payment::InvoicesController < ApplicationController
     @invoice = Invoice.find(params[:id])
   end
 
+  def new
+    @invoice = Invoice.new
+  end
+
+  def create
+    @invoice = Invoice.new(invoice_params)
+    if @invoice.save
+      redirect_to @invoice
+    else
+      render :new
+    end
+  end
 
   private
 
