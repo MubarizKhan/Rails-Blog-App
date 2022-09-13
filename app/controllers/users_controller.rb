274219ec1a09fd71b9c42974422d0f8cc02ef718
MubarizKhan <p176007@nu.edu.pk>
2022-09-13 11:47:52 +0500

    Jquery deletion not working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,22 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to root_path, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+      format.js   { render :layout => false }
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email)
+    # params.require(:article).permit(:title, :body)
+
+  end
 
 end
