e63ac0f6a90e402e6d29a3a40d6299d7b25ffd20
MubarizKhan <p176007@nu.edu.pk>
2022-09-15 19:40:59 +0500

    cleaning

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -4,13 +4,9 @@ console.log('its not hereeeeeee')
 console.log($('.my-form'))
 
 $(".my-form").on("submit", function(event){
-// $(".topic_button").on("submit", function(event){
    console.log('its hereeeeeee')
   var users = document.querySelector(".add_topic")
   var topic_val = users.value;
-
-//   var user_id = $(this).data('tp');
-//   var row = $(this);
 
   event.preventDefault();
 
@@ -18,12 +14,8 @@ $(".my-form").on("submit", function(event){
    url: "/topics/",
    type: "post",
 
-   // dataType: 'json',
-   //  accept: 'script',
-  //  dataType : JSON,
    headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
    data: { 'topic_name': topic_val },
-   // body: JSON.stringify(data),
    request : data.topic_name,
 
    success: function() {
@@ -34,9 +26,6 @@ $(".my-form").on("submit", function(event){
    },
 
   });
-//   return response.json();
-//   var response = data.topic_name;
-
 });
 
 
